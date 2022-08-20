d3f47cd9a281051203fefa9a070cce85137977a7
Samson <samson.a@esettlementgroup.com>
2022-08-20 17:39:54 +0100

    update

---
+++ src/test/java/com/example/smtpconnect/SmtpconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smtpconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmtpconnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
